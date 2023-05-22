2cae6f149cecd6ec340310ca0545c4c88522c17a
Rafael <rs.mira@campus.fct.unl.pt>
2023-05-22 12:07:25 +0100

    Passa todos os 102. Passa TLS com REST

--- tls/genAllComDNS.sh
+++ tls/genAllComDNS.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp cacerts client-ts.jks
+
 # Lista de servidores
 servidores=("users0-ourorg0" "feeds0-ourorg0" "feeds1-ourorg0" "feeds2-ourorg0" "users0-ourorg1" "feeds0-ourorg1" "feeds1-ourorg1" "feeds2-ourorg1" "users0-ourorg2" "feeds0-ourorg2" "feeds1-ourorg2" "feeds2-ourorg2")
 
@@ -36,15 +38,12 @@ EOF
 $password
 EOF
 
-    echo "Criando Truststore do Cliente"
-    cp cacerts client-ts.jks
-
+  echo "Criando Truststore do Cliente"
     # Importa o certificado para o truststore do cliente
     keytool -importcert -file "$servidor.cert" -alias "$servidor" -keystore client-ts.jks << EOF
 changeit
 yes
 EOF
 
-    # Adiciona o Subject Alternative Name (SAN) ao certificado
-    keytool -keystore "$nome_keystore" -alias "$servidor" -ext SAN=dns:"$servidor" -storepass "$password" -keypass "$password" -noprompt
 done
+
