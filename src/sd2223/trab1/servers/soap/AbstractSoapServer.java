979d0f7350dc7407b0718f404dd8d5b6d7e1b95c
Rafael <rs.mira@campus.fct.unl.pt>
2023-05-22 12:57:24 +0100

    Update 103e. Falha no 103f

--- src/sd2223/trab1/servers/soap/AbstractSoapServer.java
+++ src/sd2223/trab1/servers/soap/AbstractSoapServer.java
@@ -1,21 +1,33 @@
 package sd2223.trab1.servers.soap;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
 import jakarta.xml.ws.Endpoint;
 import sd2223.trab1.discovery.Discovery;
 import sd2223.trab1.servers.java.AbstractServer;
 import utils.IP;
 
+import javax.net.ssl.SSLContext;
+
 public class AbstractSoapServer<T> extends AbstractServer {
 	private static final String SOAP_CTX = "/soap";
 
 	final T webservice;
+
+	int port;
 	
 	protected AbstractSoapServer( boolean enableSoapDebug, Logger log, String service, int port, T webservice) {
 		super(log, service, String.format(SERVER_BASE_URI, IP.hostName(), port, SOAP_CTX));
 		this.webservice = webservice;
-		
+
+		this.port = port;
+
 		if(enableSoapDebug ) {
 			System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
 			System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
@@ -24,10 +36,28 @@ public class AbstractSoapServer<T> extends AbstractServer {
 		}
 	}
 	
-	protected void start() {
-		Endpoint.publish(serverURI.replace(IP.hostName(), INETADDR_ANY), webservice );
+	protected void start() throws NoSuchAlgorithmException, IOException {
+
+		//Endpoint.publish(serverURI.replace(IP.hostName(), INETADDR_ANY), webservice );
+
+		var server = HttpsServer.create(new InetSocketAddress(IP.hostName(), port), 0);
+
+		server.setExecutor(Executors.newCachedThreadPool());
+		server.setHttpsConfigurator(new HttpsConfigurator(SSLContext.getDefault()));
+
+		var endpoint = Endpoint.create(webservice);
+		endpoint.publish(server.createContext(SOAP_CTX));
+
+		server.start();
 
 		Discovery.getInstance().announce(service, serverURI);
 		Log.info(String.format("%s Soap Server ready @ %s\n", service, serverURI));
 	}
+
+	/*
+
+
+
+	 */
+
 }
