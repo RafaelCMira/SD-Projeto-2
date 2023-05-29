872fbec1f38578d96ca95fb6a0b7955710d984ae
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2023-05-29 02:15:07 +0100

    Retirei as classes do pull. Passa até ao 104b

--- src/sd2223/trab1/servers/proxy/ProxyFeedsServer.java
+++ src/sd2223/trab1/servers/proxy/ProxyFeedsServer.java
@@ -1,9 +1,36 @@
 package sd2223.trab1.servers.proxy;
 
-public class ProxyFeedsServer {
+import org.glassfish.jersey.server.ResourceConfig;
+import sd2223.trab1.api.java.Feeds;
+import sd2223.trab1.servers.Domain;
+import sd2223.trab1.servers.rest.AbstractRestServer;
+import sd2223.trab1.servers.rest.RestFeedsPushResource;
+import sd2223.trab1.servers.rest.RestFeedsServer;
+import utils.Args;
+
+import java.util.logging.Logger;
+
+public class ProxyFeedsServer extends AbstractRestServer {
+    public static final int PORT = 4567;
+
+    private static Logger Log = Logger.getLogger(RestFeedsServer.class.getName());
+
+    ProxyFeedsServer() {
+        super(Log, Feeds.SERVICENAME, PORT);
+    }
+
+    @Override
+    protected void registerResources(ResourceConfig config) {
+        config.register(RestFeedsPushResource.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Args.use(args);
+        Domain.set(args[0], Long.valueOf(args[1]));
+        new ProxyFeedsServer().start();
+    }
 
     // rest feeds server mas está a usar o proxy feeds resource
-
 
     /**
      *
--- src/sd2223/trab1/servers/rest/AbstractRestServer.java
+++ src/sd2223/trab1/servers/rest/AbstractRestServer.java
@@ -14,29 +14,28 @@ import javax.net.ssl.SSLContext;
 
 
 public abstract class AbstractRestServer extends AbstractServer {
-	
-	private static final String REST_CTX = "/rest";
 
-	protected AbstractRestServer(Logger log, String service, int port) {
-		super(log, service, String.format(SERVER_BASE_URI, IP.hostName(), port, REST_CTX));
-	}
+    private static final String REST_CTX = "/rest";
+
+    protected AbstractRestServer(Logger log, String service, int port) {
+        super(log, service, String.format(SERVER_BASE_URI, IP.hostName(), port, REST_CTX));
+    }
 
 
-	protected void start() {
-		try{
-			ResourceConfig config = new ResourceConfig();
+    protected void start() {
+        try {
+            ResourceConfig config = new ResourceConfig();
 
-			registerResources( config );
+            registerResources(config);
 
-			JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostName(), INETADDR_ANY)), config, SSLContext.getDefault());
+            JdkHttpServerFactory.createHttpServer(URI.create(serverURI.replace(IP.hostName(), INETADDR_ANY)), config, SSLContext.getDefault());
 
-			Discovery.getInstance().announce(service, super.serverURI);
-			Log.info(String.format("%s Server ready @ %s\n",  service, serverURI));
-		} catch (Exception e) {
+            Discovery.getInstance().announce(service, super.serverURI);
+            Log.info(String.format("%s Server ready @ %s\n", service, serverURI));
+        } catch (Exception e) {
 
-		}
+        }
+    }
 
-	}
-	
-	abstract void registerResources( ResourceConfig config );
+    protected abstract void registerResources(ResourceConfig config);
 }
