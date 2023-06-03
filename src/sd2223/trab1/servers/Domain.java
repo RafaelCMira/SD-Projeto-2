65aa5b553ac8a6bed01e1da88ef9287310a6107c
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2023-06-03 02:11:07 +0100

    update

--- src/sd2223/trab1/servers/Domain.java
+++ src/sd2223/trab1/servers/Domain.java
@@ -1,14 +1,13 @@
 package sd2223.trab1.servers;
 
 public class Domain {
+
     static String domain;
     static long uuid;
-    static long msgId;
 
     public static void set(String _domain, long _uuid) {
         domain = _domain;
         uuid = _uuid;
-        msgId = 1;
     }
 
     public static String get() {
@@ -19,11 +18,6 @@ public class Domain {
         return uuid;
     }
 
-    public synchronized static long generateMessageId() {
-        long result = msgId * 256 + uuid;
-        Domain.msgId++;
-        return result;
-    }
 
     public static boolean isRemoteUser(String user) {
         var parts = user.split("@");
